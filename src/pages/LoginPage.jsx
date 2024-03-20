68ef32336ffc0cf52f949bc70a036fa7c29db19d
rolandowebdev <rolandopranata@gmail.com>
2024-03-20 13:59:48 +0700

    chore: change title app

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ function LoginPage() {
             <article className="login-page__main">
                 <h2>
                     See <strong>The World</strong>, <br />
-                    Through Open Space App.
+                    Through Open Space.
                 </h2>
 
                 <LoginInput login={onLogin} />
